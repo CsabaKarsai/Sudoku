b4eb90d2cca45217d8069a6c2133a87d4b359682
Csaba Karsai <csaba@karsai.info>
2021-08-01 20:47:20 +0200

    added all necessary function to solve sudoku

--- code.py
+++ code.py
@@ -1,17 +1,5 @@
-#representation of a sudoku board, list of rows
+#grid1 has one solution
 grid1 = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
-
-grid2 = [
     [5,3,0,0,7,0,0,0,0],
     [6,0,0,1,9,5,0,0,0],
     [0,9,8,0,0,0,0,6,0],
@@ -23,7 +11,33 @@ grid2 = [
     [0,0,0,0,8,0,0,7,9]
 ]
 
-#prints the sudoku board in a redable format
+#grid2 has two solutions
+grid2 = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,0,0]
+]
+
+#unsolvable, check last row
+grid3 = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,7,0,8,0,0,7,9]
+]
+
+#prints the sudoku board in a readable format
 def print_grid(grid):
     for x in range(0,9):
         if x % 3 == 0 and x != 0:
@@ -37,9 +51,6 @@ def print_grid(grid):
                 print(str(grid[x][y]) + " ", end="")
 
 #checks whether the candidate number could be put at position (x,y)
-#x: 0-8
-#y: 0-8
-#number: 1-9
 def check(grid, x, y, candidate):
     if check_row(grid, x, candidate) == False:
         return False
@@ -51,13 +62,13 @@ def check(grid, x, y, candidate):
 
 #checks if the row x has a number equal to candidate
 def check_row(grid, x, candidate):
-    for i in range(0,9):
+    for i in range(9):
         if grid[x][i] == candidate:
             return False
 
 #check if the column y has a number equal to candidate
 def check_column(grid, y, candidate):
-    for i in range(0,9):
+    for i in range(9):
         if grid[i][y] == candidate:
             return False
         
@@ -67,13 +78,65 @@ def check_block(grid, x, y, candidate):
     x0 = (x // 3) * 3
     y0 = (y // 3) * 3
     #check each position in the block
-    for i in range(0,3):
-        for j in range(0,3):
+    for i in range(3):
+        for j in range(3):
             if grid[x0+i][y0+j] == candidate:
                 return False
+    
+#find an empty cell
+def find_empty(grid):
+    for x in range(9):
+        for y in range(9):
+            if grid[x][y] == 0:
+                return (x, y)
+    return None
+            
+#solve the sudoku using backtracking
+def solve(grid):
+    find = find_empty(grid)
+    if not find:
+        return True
+    x, y = find
+    for candidate in range(1,10):
+        if check(grid, x, y, candidate):
+            grid[x][y] = candidate
+            if solve(grid):
+                return True
+            grid[x][y] = 0
+    return False      
+    
+#final program
+def run(grid):
+    print("Input:")
+    print_grid(grid)
+    solvable = solve(grid)
+    if solvable:
+        print("Solution:")
+        print_grid(grid)
+    else:
+        print("This Sudoku is unsolvable!")
 
-#tests
-print_grid(grid2)
-print("-----")
-print(check(grid2, 8, 6, 1))
-print(check(grid2, 6, 8, 1))
+#run the program
+run(grid1)
+
+"""
+#alernative, printing all solutions or nothing if sudoku is unsolvable
+def solve2(grid):    
+    for x in range(9):
+        for y in range(9):
+            if grid[x][y] == 0:
+                for candidate in range(1,10):
+                    if check(grid, x, y, candidate):
+                        grid[x][y] = candidate
+                        solve2(grid)
+                        grid[x][y] = 0
+                return
+    print("Solution:")
+    print_grid(grid)
+   
+#run the program for a solvable sudoku 
+def run2(grid):
+    print("Input:")
+    print_grid(grid)
+    solve2(grid)
+"""
