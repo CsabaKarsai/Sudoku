5b158342d0250885fe55b68f0290def34e5fbcbc
Csaba Karsai <csaba@karsai.info>
2021-08-03 21:43:37 +0200

    added API call to get sudoku boards according to difficulty

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import pygame
+import requests
 
 WIDTH = 550
 HEIGHT = 550
@@ -6,6 +7,8 @@ HEIGHT = 550
 white = [255, 255, 255]
 black = [0, 0, 0]
 background_color = white
+#easy, medium, hard
+difficulty = "easy"
 
 def draw_grid_lines(win):
     for i in range(10):
@@ -15,11 +18,18 @@ def draw_grid_lines(win):
         pygame.draw.line(win, black, (50 + 50 * i, 50), (50 + 50 * i, 500), thickness)
         pygame.draw.line(win, black, (50, 50 + 50 * i), (500, 50 + 50 * i), thickness)
 
+def get_grid_from_API(difficulty):
+    response = requests.get("https://sugoku.herokuapp.com/board?difficulty=" + difficulty)
+    return response.json()["board"]
+
 def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Sudoku")
     win.fill(background_color)
+    
+    grid = get_grid_from_API("easy")
+    print(grid)
     
     draw_grid_lines(win)
     pygame.display.update()
