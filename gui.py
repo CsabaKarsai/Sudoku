455ee53d8395455049213c67dbbd90207c977cd1
Csaba Karsai <csaba@karsai.info>
2021-08-03 21:12:54 +0200

    added thickness to some grid lines

--- gui.py
+++ gui.py
@@ -4,12 +4,16 @@ WIDTH = 550
 HEIGHT = 550
 
 white = [255, 255, 255]
+black = [0, 0, 0]
 background_color = white
 
 def draw_grid_lines(win):
     for i in range(10):
-        pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
-        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
+        thickness = 1
+        if(i % 3 == 0):
+            thickness = 3
+        pygame.draw.line(win, black, (50 + 50 * i, 50), (50 + 50 * i, 500), thickness)
+        pygame.draw.line(win, black, (50, 50 + 50 * i), (500, 50 + 50 * i), thickness)
 
 def main():
     pygame.init()
