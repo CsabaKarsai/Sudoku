6d7179a9fd1a1837e3746edf82508348b8c1f6fc
Csaba Karsai <csaba@karsai.info>
2021-08-03 22:15:56 +0200

    drawing initial grid based on API call

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ white = [255, 255, 255]
 black = [0, 0, 0]
 background_color = white
 #easy, medium, hard
-difficulty = "easy"
+game_difficulty = "easy"
 
 def draw_grid_lines(win):
     for i in range(10):
@@ -22,16 +22,25 @@ def get_grid_from_API(difficulty):
     response = requests.get("https://sugoku.herokuapp.com/board?difficulty=" + difficulty)
     return response.json()["board"]
 
+def draw_grid_values(win ,grid, font):
+    for i in range(9):
+        for j in range(9):
+            if (0 < grid[i][j] < 10):
+                value = font.render(str(grid[i][j]), True, black)
+                win.blit(value, ((j + 1) * 50 + 20, (i + 1) * 50 + 14))
+
 def main():
     pygame.init()
+    pygame.font.init()
+    font = pygame.font.SysFont("comicsans", 35)
+    
     win = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Sudoku")
     win.fill(background_color)
     
-    grid = get_grid_from_API("easy")
-    print(grid)
-    
     draw_grid_lines(win)
+    grid = get_grid_from_API(game_difficulty)
+    draw_grid_values(win, grid, font)
     pygame.display.update()
     
     while True:
